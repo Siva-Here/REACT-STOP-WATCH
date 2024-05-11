6107bae226711441753be451fcff5c34e29d28e8
siva <sivahere9484@gmail.com>
2024-05-11 19:14:50 +0530

    REACT STOP_WATCH

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import StopWatch from "./StopWatch"
+function App() {
+  
+
+  return (
+    <>
+      <StopWatch/>
+      <StopWatch/>
+      <StopWatch/>
+      <StopWatch/>
+      <StopWatch/>
+      <StopWatch/>
+      <StopWatch/>
+    </>
+  )
+}
+
+export default App
